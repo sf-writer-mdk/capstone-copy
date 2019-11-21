8b07ce6ff2afefd73e14639d1e682aa4158c1021
Marvin Kirkland <sf.writer.mdk@gmail.com>
2019-11-21 10:23:39 -0800

    before font changes

--- app.js
+++ app.js
@@ -14,6 +14,97 @@ $(function() {
 
 $(window).on("scroll", function() {
     if($(window).scrollTop() > 555 && $(window).scrollTop() < 810) {
+        $(".header").addClass("actives");
+
+   } else if 
+        ($(window).scrollTop() > 810 && $(window).scrollTop() < 1287) {
+            $(".header").removeClass("actives");
+
+    } else if 
+    ($(window).scrollTop() > 1287 && $(window).scrollTop() < 2068) {
+        $(".header").addClass("actives");
+
+    } else if 
+    ($(window).scrollTop() > 2068 && $(window).scrollTop() < 3270) {
+        $(".header").removeClass("actives");
+
+    } else if 
+    ($(window).scrollTop() > 3270 && $(window).scrollTop() < 6204) {
+        $(".header").addClass("actives");
+    
+
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".header").removeClass("actives");
+    }
+    
+});
+
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 555 && $(window).scrollTop() < 810) {
+        $(".main-nav__item>a").addClass("active-scrolls");
+
+    } else if 
+    ($(window).scrollTop() > 810 && $(window).scrollTop() < 1287) {
+        $(".main-nav__item>a").removeClass("active-scrolls");
+
+    } else if 
+    ($(window).scrollTop() > 1287 && $(window).scrollTop() < 2068) {
+        $(".main-nav__item>a").addClass("active-scrolls");
+
+    } else if 
+    ($(window).scrollTop() > 2068 && $(window).scrollTop() < 3270) {
+        $(".main-nav__item>a").removeClass("active-scrolls");
+
+    } else if 
+    ($(window).scrollTop() > 3270 && $(window).scrollTop() < 6200) {
+        $(".main-nav__item>a").addClass("active-scrolls");
+
+        
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".main-nav__item>a").removeClass("active-scrolls");
+    }
+});
+
+
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 555 && $(window).scrollTop() < 810) {
+        $(".main-nav__item--cta>a").addClass("active-contacts");
+
+    } else if 
+    ($(window).scrollTop() > 810 && $(window).scrollTop() < 1287) {
+        $(".main-nav__item--cta>a").removeClass("active-contacts");
+
+    } else if 
+    ($(window).scrollTop() > 1287 && $(window).scrollTop() < 2068) {
+        $(".main-nav__item--cta>a").addClass("active-contacts");
+
+    } else if 
+    ($(window).scrollTop() > 2068 && $(window).scrollTop() < 3270) {
+        $(".main-nav__item--cta>a").removeClass("active-contacts");
+
+    } else if 
+    ($(window).scrollTop() > 3270 && $(window).scrollTop() < 6200) {
+        $(".main-nav__item--cta>a").addClass("active-contacts");
+
+        
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".main-nav__item--cta>a").removeClass("active-contacts");
+    }
+});
+
+
+
+
+
+
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 555 && $(window).scrollTop() < 810) {
         $(".header").addClass("active");
 
    } else if 
@@ -39,6 +130,7 @@ $(window).on("scroll", function() {
     }
     
 });
+
 
 
 
